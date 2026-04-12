5d56ac7b821b3c4d9b601238b9fe2e6c5f69f9c3
Benjamin Coriou <benjamin@coriou.net>
2026-04-12 03:10:47 +0200

    feat: add melody progression to starsailz pattern

--- patterns/starsailz.js
+++ patterns/starsailz.js
@@ -6,4 +6,12 @@ drums: stack(
   s("~ hh ~ hh").bank("akaimpc60").pan(sine.fast(8)),
   s("hh*8").bank("mpc1000").pan(sine.slow(4)),
 )
+
+melody: progression('C Am Dm G')
+  .slice([0, 0.25, 0.5, 0.75], "0 1 1 <2 3>")
+  .s("gm_piano")
+  .release(2)
+  .room(0.9)
+  .chorus(0.6)
+  .gain(isaw.range(0, 0.6).fast(4))
 `;
