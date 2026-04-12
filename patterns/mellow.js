8c1942fc839983cb097b085e9c8cb49cff7fec64
Benjamin Coriou <benjamin@coriou.net>
2026-04-12 16:11:52 +0200

    feat: optimize melody progression by refining room settings and consolidating slicing logic

--- patterns/mellow.js
+++ patterns/mellow.js
@@ -15,44 +15,49 @@ const chords = 'C Am C G'
 const chords_alt_a = 'C ~ C ~'
 const chords_alt_b = '~ Am ~ G'
 
+const slicing = [2, "0 1 <2 2> 3"]
+
 melo: stack(
-  progression(chords).room(0.7).roomsize(2).s("gm_lead_8_bass_lead").lpf(800),
+  progression(chords).s("gm_lead_8_bass_lead").lpf(800),
 
   stack(
-    progression(chords_alt_a)
+    progression(chords_alt_a, { rhythm: "comp" })
       .s("gm_pad_poly")
       .lpf(sine.range(1000, 800))
       .pan(sine.slow(4)),
-    progression(chords_alt_b)
+    progression(chords_alt_b, { rhythm: "comp" })
       .s("gm_pad_poly")
       .lpf(sine.range(800, 1000))
       .pan(sine.slow(8)),
   )
+    .room(0.25)
+    .roomsize(10)
     .lpdepth(0.25)
     .lpdc(2)
     .lpa(0.5)
     .lpenv("<3 2 1 0 -1 -2 -4>/4")
     .gain(sine.range(0.5, 0.75)),
 )
-  .slice(2, "0 1 <2 2> 3")
+  .slice(...slicing)
   .gain(sine.range(0.7, 0.9))
 
 bells: progression(chords)
-  .slice(2, "0 1 <2 2> 3")
+  .slice(...slicing)
   .decay(0.25)
   .sustain(0.9)
   .s("gm_tubular_bells")
   .lpf(sine.range(800, 1200))
   .pan(sine.slow(4))
   .gain(0.5)
+  .mask("<0 0 1 0 1 0 0 0 1 0 0 1 0 0 0 1>")
 
 choir: stack(
   progression(chords_alt_a)
-    .slice(2, "0 1 <2 2> 3")
+    .slice(...slicing)
     .s("gm_choir_aahs")
     .pan(sine.slow(8)),
   progression(chords_alt_b)
-    .slice(2, "0 1 <2 2> 3")
+    .slice(...slicing)
     .s("gm_choir_aahs")
     .decay(0.3)
     .sustain(0.9)
